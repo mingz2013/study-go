6f50d553a4a3c65f300398e82f53431759a0b38f
mingz <305603665@qq.com>
2017-07-06 21:51:28 +0800

    init

--- log_analysis/src/plugins/wanfa_tongji.go
+++ log_analysis/src/plugins/wanfa_tongji.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func ExcuteWanfa(lines []map[string]interface{}) (result map[string]interface{}) {
 	for i := 0; i < len(lines); i++ {
-		var item_params, play_mode string
-		item_params = lines[i]["item_params"]
-		play_mode = lines[i]["play_mode"]
+		//var item_params, play_mode string
+		item_params := lines[i]["item_params"]
+		play_mode := lines[i]["play_mode"]
 
 		fmt.Println(item_params, play_mode)
 	}
+	return
 }
--- log_analysis/src/utils/table_config.go
+++ log_analysis/src/utils/table_config.go
@@ -1,9 +1,9 @@
 package utils
 
 func GetKeyDesc(play_mode, param_type string) (desc string) {
-
+	return
 }
 
 func GetalueDesc(play_mode, param_type string, id int) (desc string) {
-
+	return
 }
