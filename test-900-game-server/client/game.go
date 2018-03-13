ca8c9c150ceb3372ef388a3a662cd0cf8775bd62
mingz <305603665@qq.com>
2018-03-13 15:55:34 +0800

    echo

--- test-900-game-server/client/game.go
+++ test-900-game-server/client/game.go
@@ -3,9 +3,14 @@ package client
 import (
 	"net"
 	"fmt"
+	"io"
+	"os"
 )
 
 func HandleConn(conn net.Conn) {
 	fmt.Printf(conn.RemoteAddr().String() + "\n")
-
+	b := []byte(`hello`)
+	conn.Write(b)
+	io.Copy(os.Stdout, conn)
+	conn.Close()
 }
--- test-900-game-server/gate/run.go
+++ test-900-game-server/gate/run.go
@@ -6,14 +6,15 @@ import (
 	"strconv"
 	"fmt"
 	"net"
+	"io"
 )
 
 func HandleConn(conn net.Conn) {
 
 	fmt.Printf(conn.RemoteAddr().String() + "\n")
 
-	b := []byte(`hello`)
-	conn.Write(b)
+	io.Copy(conn, conn)
+	conn.Close()
 }
 
 func Run() {
