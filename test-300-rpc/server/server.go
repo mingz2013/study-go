6b263c362d0f11a4340e97df687e9643d2f7075d
mingz <305603665@qq.com>
2018-03-15 20:56:00 +0800

    rpc

---
+++ test-300-rpc/server/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+	"net/rpc"
+	"net"
+	"log"
+	"net/http"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+	arith := new(Arith)
+
+	rpc.Register(arith)
+
+	rpc.HandleHTTP()
+
+	l, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go http.Serve(l, nil)
+}
