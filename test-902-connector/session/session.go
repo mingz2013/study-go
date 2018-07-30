b188ff4f8d8985ae341d33f609a74d2b39f6dea2
mingz <305603665@qq.com>
2018-07-30 09:24:39 +0800

    connector

--- test-902-connector/session/session.go
+++ test-902-connector/session/session.go
@@ -8,3 +8,21 @@ type Session struct {
 	conn   net.Conn
 	userId int
 }
+
+func (s *Session) Send() {
+
+}
+
+func (s *Session) Recv() {
+
+}
+
+func (s *Session) SendMsg() {
+
+}
+
+func (s *Session) RecvMsg() {
+
+}
+
+
